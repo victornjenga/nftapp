6f4ec0f29a4be924e7f3d68ec27066ff68832977
Vic Tor <you@example.com>
2022-07-19 16:17:09 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from "expo-font";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import Home from "./screens/Home.";
+import Details from "./screens/Details";
+
+const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme,
+  color: {
+    ...DefaultTheme.colors,
+    backgroundColor: "transparent",
+  },
+};
+export default function App() {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+  if (!loaded) return null;
+
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Homez"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ components/NFTCard.js
@@ -0,0 +1,63 @@
+import { View, Image, Text } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { COLORS, SIZES, assets, SHADOWS } from "../constants";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMethod="cover"
+          border
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+      </View>
+      <CircleButton imgUrl={assets.heart} right={10} top={10} />
+
+      <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default NFTCard;
